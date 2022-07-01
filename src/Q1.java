f9e0ba80ae9e5276c3f7fed0e4de28a41f462700
shocker945516 <shocker945516@gmail.com>
2022-07-01 11:50:44 -0700

    done

--- src/Q1.java
+++ src/Q1.java
@@ -8,12 +8,13 @@ public class Q1 {
         Scanner input = new Scanner(System.in);
         Random rand = new Random();
         ArrayList<Integer> arrList = new ArrayList<>();
+
         for (int i = 0; i < 10; i++) {
             arrList.add(rand.nextInt((50) + 1));
         }
         System.out.println(arrList);
         System.out.println("Give me a number");
-        int aNum = input.nextInt();
+        Integer aNum = input.nextInt();
         if (arrList.contains(aNum)) {
             System.out.println("The number is in the list.");
         } else {
--- src/Q3.java
+++ src/Q3.java
@@ -1,18 +1,59 @@
 import java.util.ArrayList;
-import java.util.List;
 
 public class Q3 {
     public static void main(String[] args) {
 
-        Car car1 = new Car("Missing Link", "Asurada GSX", 1991);
-        Car car2 = new Car("Tesla", "Model 3", 2017);
-        Car car3 = new Car("Ford", "Model T", 1908);
+        ArrayList<Car> carList = new ArrayList<>();
+        carList.add(new Car("Missing Link", "Asurada GSX", 1991));
+        carList.add(new Car("Tesla", "Model 3", 2017));
+        carList.add(new Car("Ford", "Model T", 1908));
 
-        ArrayList<Integer> carList = new ArrayList<>();
-        carList.add(car1.getYear());
-        carList.add(car2.getYear());
-        carList.add(car3.getYear());
+        carList.sort(null);
 
+        for (Car car : carList) {
+            System.out.println("Element:" + car);
+        }
+    }
+
+}
+
+class Car implements Comparable<Car> {
+    String make;
+    String model;
+    int year;
+
+    public Car(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    @Override
+    public String toString() {
+        return "Car [make=" + make + ", model=" + model + ", year=" + year + "]";
+    }
+
+    @Override
+    public int compareTo(Car o) {
+        if (year == o.year) {
+            return 0;
+        } else if (year > o.year) {
+            return 1;
+        } else {
+            return -1;
+        }
     }
 
 }
